4530f21813e688feb65f23e2a9e1bc6581240f56
Hugo Lima <hugooolima@gmail.com>
2020-05-01 20:08:10 -0300

    Correção de Row e Col

--- src/paginas/main/index.js
+++ src/paginas/main/index.js
@@ -1,32 +1,17 @@
 import React from "react";
-import "./estilo.css";
-import {Container, Col, Row, Div, Label} from 'reactstrap';
+import { Col, Row } from "reactstrap";
 import Cinvestidor from "./card/cardInvestidor";
 
-const main = () => {
-
+import { Container } from "./styles";
+export default function Main() {
   return (
-    <div style={{ backgroundColor: "#f1f1f1", width: "100%", height: "100vh" }}>
-      <Container className="main">
-
-            <Row className="ColInvestidor">
-                <Col><h1 id="cabecalho">Meu Investidor</h1>
-                <Row><Cinvestidor /></Row>
-                </Col>
-            </Row>
-            
-
-            <Col>
-                  <Row className="minha-carteira">carteira</Row>
-            </Col>
-
-            <Col>
-                  <Row className="meus-invetimentos">invetimentos</Row>
-            </Col>
-          
-
-      </Container>
-    </div>
+    <Container>
+      <Row>
+        <Col md={4}>
+          <Cinvestidor />
+        </Col>
+        <Col md={8}>Lado 2</Col>
+      </Row>
+    </Container>
   );
-};
-export default main;
+}
