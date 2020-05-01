054fc8fc185ac2c8a0fe642ef5697c6057bdba7c
Hugo Lima <hugooolima@gmail.com>
2020-05-01 20:08:39 -0300

    Novo estilo do card "Meu Consultor"

---
+++ src/paginas/main/styles.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background: #f1f1f1;
+  width: 100%;
+  min-height: 100vh;
+  padding: 50px;
+`;
+export const TitleConsoltor = styled.div`
+  width: 100%;
+  padding: 10px 0;
+  background: #46da96;
+  color: #ffffff;
+  font-size: 23px;
+`;
+export const ConsultorImg = styled.img`
+  border-radius: 50%;
+  width: 150px;
+`;
+export const Center = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+export const Detail = styled.p`
+  color: #555555;
+  font-style: italic;
+`;
+export const Description = styled.div`
+  padding: 5px 30px;
+`;
