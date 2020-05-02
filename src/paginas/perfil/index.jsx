8d4e17a0b6a7f97c6fc27230e74218ea1b059301
Hugo Lima <hugooolima@gmail.com>
2020-05-01 22:22:28 -0300

    Pagina de perfil do usuário

--- src/paginas/perfil/index.jsx
+++ src/paginas/perfil/index.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { Card, CardBody, Row, Col } from "reactstrap";
+import { Row, Col } from "reactstrap";
+
+import CardPessoal from "./card/CardPessoal";
+import CardInvestidor from "./card/CardInvestidor";
 
 import { Container } from "./styles";
 
@@ -7,8 +10,12 @@ export default function Perfil() {
   return (
     <Container>
       <Row>
-        <Col md={4}></Col>
-        <Col md={8}></Col>
+        <Col md={4}>
+          <CardPessoal />
+        </Col>
+        <Col md={8}>
+          <CardInvestidor />
+        </Col>
       </Row>
     </Container>
   );
